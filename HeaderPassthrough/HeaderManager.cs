2c4e5b70c0e2d5bd68563c8fe286689db0d7a0d5
Johan Olofsson <johaolof@gmail.com>
2016-02-09 21:06:05 +0100

    Now avoids AD lookup for paths ending with manifest.appcache, not all paths containing the word manifest.

--- HeaderPassthrough/HeaderManager.cs
+++ HeaderPassthrough/HeaderManager.cs
@@ -50,66 +50,68 @@ namespace LogonPassthrough
 
         public void ProcessRequest(Object source, EventArgs e)
         {
-            HttpApplication application = (HttpApplication) source;
+            HttpApplication application = (HttpApplication)source;
             HttpRequest req = application.Context.Request;
-            if(!req.Path.Contains("manifest")) {
-                if (req.ServerVariables.AllKeys.Contains("LOGON_USER"))
+            if (req.Path.EndsWith("manifest.appcache"))
+            {
+                return;
+            }
+            if (req.ServerVariables.AllKeys.Contains("LOGON_USER"))
+            {
+                String logon_user = application.Context.Request.ServerVariables.Get("LOGON_USER");
+                String[] logon_user_parts = logon_user.Split('\\');
+                if (logon_user_parts.Length == 2)
                 {
-                    String logon_user = application.Context.Request.ServerVariables.Get("LOGON_USER");
-                    String[] logon_user_parts = logon_user.Split('\\');
-                    if (logon_user_parts.Length == 2)
+                    // Should be something like EU, or SVEALIDEN in my test setup.
+                    String domain = logon_user_parts[0];
+                    // Should be Administrator or jolofsson or something like that.
+                    String username = logon_user_parts[1];
+
+                    // Let's get a context for the domain this user belongs to.
+                    PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, domain);
+                    Principal user = new UserPrincipal(principalContext); // Empty user to use for our search
+                    user.SamAccountName = username;
+                    PrincipalSearcher searcher = new PrincipalSearcher(user);
+                    UserPrincipal user_principal = (UserPrincipal)searcher.FindOne();
+                    if (user_principal != null)
                     {
-                        // Should be something like EU, or SVEALIDEN in my test setup.
-                        String domain = logon_user_parts[0];
-                        // Should be Administrator or jolofsson or something like that.
-                        String username = logon_user_parts[1];
-
-                        // Let's get a context for the domain this user belongs to.
-                        PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, domain);
-                        Principal user = new UserPrincipal(principalContext); // Empty user to use for our search
-                        user.SamAccountName = username;
-                        PrincipalSearcher searcher = new PrincipalSearcher(user);
-                        UserPrincipal user_principal = (UserPrincipal) searcher.FindOne();
-                        if (user_principal != null)
+                        var headers = application.Context.Request.Headers;
+                        if (user_principal.EmailAddress == null || user_principal.EmailAddress.Equals("no_mail"))
                         {
-                            var headers = application.Context.Request.Headers;
-                            if (user_principal.EmailAddress == null || user_principal.EmailAddress.Equals("no_mail"))
-                            {
-                                // Unusual case, probably only for admin users. Safe fallback?
-                                addHeader(headers, "X-Logon-AccountName", user_principal.SamAccountName);
-                            }
-                            else
-                            {
-                                addHeader(headers, "X-Logon-AccountName", user_principal.EmailAddress);
-                            }
-                            addHeader(headers, "X-Logon-DistinguishedName", user_principal.DistinguishedName);
-                            addHeader(headers, "X-Logon-EmailAddress", user_principal.EmailAddress);
-                            addHeader(headers, "X-Logon-DisplayName", user_principal.DisplayName);
-                            addHeader(headers, "X-Logon-DomainUserName", user_principal.SamAccountName);
-
-                            if (user_principal.GetGroups() != null)
-                            {
-                                String groups = "";
-                                foreach (GroupPrincipal group in user_principal.GetGroups())
-                                {
-                                    groups += group.ToString() + ",";
-                                }
-                                int lastChar = groups.Length - 1;
-                                if (groups[lastChar].Equals(','))
-                                {
-                                    groups = groups.Remove(lastChar);
-                                }
-                                headers.Add("X-Logon-Groups", groups);
-                            }
-                            else
-                            {
-                                headers.Add("X-Logon-Groups", NOT_SET);
-                            }
+                            // Unusual case, probably only for admin users. Safe fallback?
+                            addHeader(headers, "X-Logon-AccountName", user_principal.SamAccountName);
                         }
                         else
                         {
-                            throw new HttpException(403, "No such user principal");
+                            addHeader(headers, "X-Logon-AccountName", user_principal.EmailAddress);
                         }
+                        addHeader(headers, "X-Logon-DistinguishedName", user_principal.DistinguishedName);
+                        addHeader(headers, "X-Logon-EmailAddress", user_principal.EmailAddress);
+                        addHeader(headers, "X-Logon-DisplayName", user_principal.DisplayName);
+                        addHeader(headers, "X-Logon-DomainUserName", user_principal.SamAccountName);
+
+                        if (user_principal.GetGroups() != null)
+                        {
+                            String groups = "";
+                            foreach (GroupPrincipal group in user_principal.GetGroups())
+                            {
+                                groups += group.ToString() + ",";
+                            }
+                            int lastChar = groups.Length - 1;
+                            if (groups[lastChar].Equals(','))
+                            {
+                                groups = groups.Remove(lastChar);
+                            }
+                            headers.Add("X-Logon-Groups", groups);
+                        }
+                        else
+                        {
+                            headers.Add("X-Logon-Groups", NOT_SET);
+                        }
+                    }
+                    else
+                    {
+                        throw new HttpException(403, "No such user principal");
                     }
                 }
             }
